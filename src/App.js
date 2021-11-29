446dfc008c8fd34a26ec96deb5a8ead49cf0ea1f
eugenjamb <72166090+eugenjamb@users.noreply.github.com>
2021-11-29 20:59:26 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import HomePage from './components/homepage.component';
+
+import './css/App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <HomePage />
+      </div>
+    );
+  }
+}
+
+export default App;
