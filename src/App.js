817b481308faeb7c5aa8b58011e420f3824769fb
eugenjamb <72166090+eugenjamb@users.noreply.github.com>
2021-12-01 00:50:13 +0000

    new shop page added

--- src/App.js
+++ src/App.js
@@ -2,22 +2,18 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import HomePage from './page/homepage/homepage.component';
+import ShopPage from './page/shop/shop.component';
+import Header from './components/header/header.component';
 
 import './css/App.css';
-
-const HatsPage = () => {
-  return (
-    <div>sadasd</div>
-  )
-}
-
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
