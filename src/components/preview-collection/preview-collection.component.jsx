702fd76668bcb6f53aa80ba5e51b2021bf4dd711
eugenjamb <72166090+eugenjamb@users.noreply.github.com>
2021-12-11 05:23:15 +0000

    redux thunk added to get the store data from firestore

--- src/components/preview-collection/preview-collection.component.jsx
+++ src/components/preview-collection/preview-collection.component.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
 import CollectionItem from '../collection-item/collection-item.component';
 
 import './preview-collection.styles.scss';
 
-const PreviewCollection = ({ title, items }) => (
+const PreviewCollection = ({ title, items, history, match, routeName}) => (
   <div className='collection-preview'>
-    <h1 className='title'>{title.toUpperCase()}</h1>
+    <h1 className='title' onClick={() => history.push(`${match.path}/${routeName}`)}>{title.toUpperCase()}</h1>
     <div className='preview'>
       {items
         .filter((item, idx) => idx < 4)
         .map((item) => (
           <CollectionItem key={item.id} item={item} />
         ))}
-    </div>
+    </div> 
   </div>
 ); 
 
-export default PreviewCollection;
+export default withRouter(PreviewCollection);
