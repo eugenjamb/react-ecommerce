218a32039092e7180a9910e8b27cd1bfa4273256
eugenjamb <72166090+eugenjamb@users.noreply.github.com>
2021-12-14 04:07:12 +0000

    dispatch hook for app comp added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
-import { connect } from "react-redux";
-import { createStructuredSelector } from "reselect";
+import { useSelector, useDispatch } from "react-redux";
 
 import HomePage from "./page/homepage/homepage.component";
 import ShopPage from "./page/shop/shop.component";
@@ -14,10 +13,15 @@ import { selectCurrentUser } from "./redux/user/user.selectors";
 
 import "./css/App.css";
 
-const App = ({ checkUserSession, currentUser }) => {
+const App = () => {
+
+  const currentUser = useSelector(selectCurrentUser);
+  const dispatch = useDispatch();
+
+
   useEffect(() => {
-    checkUserSession();
-  }, [checkUserSession]);
+    dispatch(checkUserSession());
+  }, [dispatch]);
 
   return (
     <div>
@@ -38,12 +42,5 @@ const App = ({ checkUserSession, currentUser }) => {
   );
 };
 
-const mapStateToProps = createStructuredSelector({
-  currentUser: selectCurrentUser,
-});
 
-const mapDispatchToProps = (dispatch) => ({
-  checkUserSession: () => dispatch(checkUserSession()),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
