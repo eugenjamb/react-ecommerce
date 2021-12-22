b473cfda0df0f632bef90601a478bc89b095f30b
eugenjamb <72166090+eugenjamb@users.noreply.github.com>
2021-12-22 06:31:45 +0000

    now cart items are backed and fetched from firestore

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(compression());
 
 if (process.env.NODE_ENV === "production") {
+  app.use(compression());
   app.use(enforce.HTTPS({ trustProtoHeader: true }));
   app.use(express.static(path.join(__dirname, "client/build")));
 
